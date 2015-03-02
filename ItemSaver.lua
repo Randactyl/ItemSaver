e0fc040f4e06ee30178f7ac43775c5a7a63167d3
Randactyl <rafowler@randactyl.com>
2015-03-02 09:56:55 -0800

    initial commit - addon version 1.2.0.0

---
+++ ItemSaver.lua
@@ -0,0 +1,370 @@
+local libFilters = LibStub("libFilters")
+
+local BACKPACK = ZO_PlayerInventoryBackpack
+local BANK = ZO_PlayerBankBackpack
+local GUILD_BANK = ZO_GuildBankBackpack
+local DECONSTRUCTION = ZO_SmithingTopLevelDeconstructionPanelInventoryBackpack
+local ENCHANTING = ZO_EnchantingTopLevelInventoryBackpack
+local LIST_DIALOG = ZO_ListDialog1List
+
+local ISSettings = nil
+local markedItems = nil
+
+local SIGNED_INT_MAX = 2^32 / 2 - 1
+local INT_MAX = 2^32
+
+--converts unsigned itemId to signed
+local function SignItemId(itemId)
+	if(itemId and itemId > SIGNED_INT_MAX) then
+		itemId = itemId - INT_MAX
+	end
+	return itemId
+end
+
+local function GetInfoFromRowControl(rowControl)
+	--gotta do this in case deconstruction...
+	local dataEntry = rowControl.dataEntry
+	local bagId, slotIndex 
+
+	--case to handle equiped items
+	if(not dataEntry) then
+		bagId = rowControl.bagId
+		slotIndex = rowControl.slotIndex
+	else
+		bagId = dataEntry.data.bagId
+		slotIndex = dataEntry.data.slotIndex
+	end
+
+	--case to handle list dialog, list dialog uses index instead of slotIndex and bag instead of badId...?
+	if(dataEntry and not bagId and not slotIndex) then 
+		bagId = rowControl.dataEntry.data.bag
+		slotIndex = rowControl.dataEntry.data.index
+	end
+
+	return bagId, slotIndex
+end
+
+local function GetItemSaverControl(parent)
+	return parent:GetNamedChild("ItemSaver")
+end
+
+local function CreateMarkerControl(parent)
+	local control = parent:GetNamedChild("ItemSaver")
+	local bagId, slotIndex = GetInfoFromRowControl(parent)
+	local signedItemInstanceId = SignItemId(GetItemInstanceId(bagId, slotIndex))
+
+	if(not control) then
+		control = WINDOW_MANAGER:CreateControl(parent:GetName() .. "ItemSaver", parent, CT_TEXTURE)
+		control:SetDimensions(32, 32)
+	end
+
+	control:SetTexture(ISSettings:GetTexturePath())
+	control:SetColor(ISSettings:GetTextureColor())
+
+	if(markedItems[signedItemInstanceId]) then
+		control:SetHidden(false)
+	else
+		control:SetHidden(true)
+	end
+
+	if(parent:GetWidth() - parent:GetHeight() < 5) then
+		if(parent:GetNamedChild("SellPrice")) then
+			parent:GetNamedChild("SellPrice"):SetHidden(true)
+		end
+		control:SetDrawTier(DT_HIGH)
+		control:ClearAnchors()
+		control:SetAnchor(CENTER, parent, BOTTOMLEFT, 12, -12)
+	else
+		control:ClearAnchors()
+		control:SetAnchor(LEFT, parent, LEFT)
+	end
+
+	return control
+end
+
+local function CreateMarkerControlForEquipment(parent)
+	local control = CreateMarkerControl(parent)
+	control:ClearAnchors()
+	control:SetAnchor(BOTTOMLEFT, parent, BOTTOMLEFT)
+	control:SetDimensions(20, 20)
+	control:SetDrawTier(1)
+end
+
+local function RefreshEquipmentControls()
+	for i=1, ZO_Character:GetNumChildren() do
+		local child = ZO_Character:GetChild(i)
+		if(child and child:GetName():find("ZO_CharacterEquipmentSlots")) then
+			CreateMarkerControlForEquipment(ZO_Character:GetChild(i))
+		end
+	end
+end
+
+local function RefreshAll()
+	ZO_ScrollList_RefreshVisible(BACKPACK)
+	ZO_ScrollList_RefreshVisible(BANK)
+	ZO_ScrollList_RefreshVisible(GUILD_BANK)
+	ZO_ScrollList_RefreshVisible(DECONSTRUCTION)
+	ZO_ScrollList_RefreshVisible(ENCHANTING)
+	ZO_ScrollList_RefreshVisible(LIST_DIALOG)
+	RefreshEquipmentControls()
+end
+
+local function UnsaveItem(bagId, slotIndex)
+	local signedItemInstanceId = SignItemId(GetItemInstanceId(bagId, slotIndex))
+
+	if(markedItems[signedItemInstanceId]) then
+		markedItems[signedItemInstanceId] = nil
+	end
+	RefreshAll()
+end
+
+local function SaveItem(bagId, slotIndex)
+	local signedItemInstanceId = SignItemId(GetItemInstanceId(bagId, slotIndex))
+
+	markedItems[signedItemInstanceId] = true
+	RefreshAll()
+end
+
+local function ToggleSave(bagId, slotIndex)
+	local signedItemInstanceId = SignItemId(GetItemInstanceId(bagId, slotIndex))
+
+	if(not markedItems[signedItemInstanceId]) then 
+		SaveItem(bagId, slotIndex)
+		return true
+	end
+	UnsaveItem(bagId, slotIndex)
+	return false
+end
+
+local function AddContextMenuOption(rowControl)
+	local bagId, slotIndex = GetInfoFromRowControl(rowControl)
+	local signedItemInstanceId = SignItemId(GetItemInstanceId(bagId, slotIndex))
+
+	if(not markedItems[signedItemInstanceId]) then 
+		AddMenuItem("Save item", function() 
+				SaveItem(bagId, slotIndex) 
+
+				if(GetItemSaverControl(rowControl)) then
+					GetItemSaverControl(rowControl):SetHidden(false)
+				end
+			end, MENU_ADD_OPTION_LABEL)
+	else
+		AddMenuItem("Unsave item", function()
+				UnsaveItem(bagId, slotIndex)
+
+				if(GetItemSaverControl(rowControl)) then
+					GetItemSaverControl(rowControl):SetHidden(true)
+				end
+			end, MENU_ADD_OPTION_LABEL)
+	end
+	ShowMenu(self)
+end
+
+local function AddContextMenuOptionSoon(rowControl)
+	if(rowControl:GetOwningWindow() == ZO_TradingHouse) then return end
+	if(BACKPACK:IsHidden() and BANK:IsHidden() and GUILD_BANK:IsHidden() and DECONSTRUCTION:IsHidden() and ENCHANTING:IsHidden()) then return end
+
+	if(rowControl:GetParent() ~= ZO_Character) then
+		zo_callLater(function() AddContextMenuOption(rowControl:GetParent()) end, 50)
+	else
+		zo_callLater(function() AddContextMenuOption(rowControl) end, 50)
+	end
+end
+
+local function FilterSavedItems(bagId, slotIndex, ...)
+	if(markedItems[SignItemId(GetItemInstanceId(bagId, slotIndex))]) then
+		return false
+	end
+	return true
+end
+
+local function FilterSavedItemsForShop(slot)
+	local bagId, slotIndex = GetInfoFromRowControl(slot)
+	local signedItemInstanceId = SignItemId(GetItemInstanceId(bagId, slotIndex))
+
+	if(markedItems[signedItemInstanceId]) then
+		return false
+	end
+	return true
+end
+
+function ItemSaver_ToggleDeconstructionFilter( toggle )
+	if(toggle) then
+		if(ISSettings:IsFilterDeconstruction() and not libFilters:IsFilterRegistered("ItemSaver_DeconstructionFilter")) then
+			libFilters:RegisterFilter("ItemSaver_DeconstructionFilter", LAF_DECONSTRUCTION, FilterSavedItems)
+		end
+	else
+		libFilters:UnregisterFilter("ItemSaver_DeconstructionFilter")
+	end
+end
+
+function ItemSaver_ToggleEnchantingFilter( toggle )
+	if(toggle) then
+		if(ISSettings:IsFilterDeconstruction() and not libFilters:IsFilterRegistered("ItemSaver_EnchantingFilter")) then
+			libFilters:RegisterFilter("ItemSaver_EnchantingFilter", LAF_ENCHANTING_EXTRACTION, FilterSavedItems)
+		end
+	else
+		libFilters:UnregisterFilter("ItemSaver_EnchantingFilter")
+	end
+end
+
+function ItemSaver_ToggleShopFilter( toggle )
+	if(toggle) then
+		if(ISSettings:IsFilterShop() and not libFilters:IsFilterRegistered("ItemSaver_ShopFilter")) then
+			libFilters:RegisterFilter("ItemSaver_ShopFilter", LAF_STORE, FilterSavedItemsForShop)
+		end
+	else
+		libFilters:UnregisterFilter("ItemSaver_ShopFilter")
+	end
+end
+
+function ItemSaver_ToggleFilters( toggle, quiet )
+	if(not quiet) then
+		if(not toggle) then
+			d("ItemSaver filters turned OFF")
+		else
+			d("ItemSaver filters turned ON")
+		end
+	end
+	ItemSaver_ToggleShopFilter(toggle)
+	ItemSaver_ToggleDeconstructionFilter(toggle)
+	ItemSaver_ToggleEnchantingFilter(toggle)
+	--ZO_ScrollList_RefreshVisible(LIST_DIALOG)
+	RefreshAll()
+end
+
+--[[GLOBAL FUNCTIONS]]------------------------------------------------------------------
+--returns true if the shop should be filterd, returns false otherwise
+function ItemSaver_IsShopFiltered()
+	return ISSettings:IsFilterOn() and ISSettings:IsFilterShop()
+end
+
+--returns true if the deconstruction panel should be filterd, returns false otherwise
+function ItemSaver_IsDeconstructionFiltered()
+	return ISSettings:IsFilterOn() and ISSettings:IsFilterDeconstruction()
+end
+
+--returns true if the research panel should be filterd, returns false otherwise
+function ItemSaver_IsResearchFiltered()
+	return ISSettings:IsFilterOn() and ISSettings:IsFilterResearch()
+end
+
+--returns true if the item is saved, returns nil if the item is not saved
+function ItemSaver_IsItemSaved(bagId, slotIndex)
+	return markedItems[SignItemId(GetItemInstanceId(bagId, slotIndex))]
+end
+
+function ItemSaver_ToggleItemSave(bagId, slotIndex)
+	local returnVal
+	--if bagId is nil, then keybind was pressed.
+	if bagId == nil then
+		local mouseOverControl = WINDOW_MANAGER:GetMouseOverControl()
+		--if is a backpack row or child of one
+		if(mouseOverControl:GetName():find("^ZO_%a+Backpack%dRow%d%d*")) then
+			--check if the control IS the row
+			if(mouseOverControl:GetName():find("^ZO_%a+Backpack%dRow%d%d*$")) then
+				local bagId, slotIndex = GetInfoFromRowControl(mouseOverControl)
+
+				returnVal = ToggleSave(bagId, slotIndex)
+			else
+				mouseOverControl = mouseOverControl:GetParent()
+				--this SHOULD be the row control - if it isn't then idk how to handle it without going iterating through parents
+				--that shouldn't happen unless someone is doing something weird
+				if(mouseOverControl:GetName():find("^ZO_%a+Backpack%dRow%d%d*$")) then
+					local bagId, slotIndex = GetInfoFromRowControl(mouseOverControl)
+
+					returnVal = ToggleSave(bagId, slotIndex)
+				end
+			end
+		elseif(mouseOverControl:GetName():find("^ZO_CharacterEquipmentSlots.+$")) then
+			local bagId, slotIndex = GetInfoFromRowControl(mouseOverControl)
+
+			returnVal = ToggleSave(bagId, slotIndex)
+		end
+	else
+		--if bagId is not nil, then function was called by another addon.
+		returnVal = ToggleSave(bagId, slotIndex)
+	end
+
+	return returnVal
+end
+--[[END GLOBAL FUNCTIONS]]--------------------------------------------------------------
+
+local function ItemSaver_Loaded(eventCode, addOnName)
+	if(addOnName ~= "ItemSaver") then return end
+
+	ISSettings = ItemSaverSettings:New()
+	markedItems = ISSettings:GetMarkedItems()
+
+    --Wobin, if you're reading this: <3
+    for _,v in pairs(PLAYER_INVENTORY.inventories) do
+		local listView = v.listView
+		if listView and listView.dataTypes and listView.dataTypes[1] then
+			local hookedFunctions = listView.dataTypes[1].setupCallback				
+			
+			listView.dataTypes[1].setupCallback = 
+				function(rowControl, slot)						
+					hookedFunctions(rowControl, slot)
+					CreateMarkerControl(rowControl)
+				end				
+		end
+	end
+
+	ZO_PreHook("ZO_InventorySlot_ShowContextMenu", AddContextMenuOptionSoon)
+	ZO_PreHook("PlayOnEquippedAnimation", CreateMarkerControlForEquipment)
+
+	RefreshEquipmentControls()
+
+	--deconstruction hook
+	local hookedFunctions = DECONSTRUCTION.dataTypes[1].setupCallback
+	DECONSTRUCTION.dataTypes[1].setupCallback = function(rowControl, slot)
+			hookedFunctions(rowControl, slot)
+			CreateMarkerControl(rowControl)
+		end
+
+	--enchanting hook
+	local hookedFunctions = ENCHANTING.dataTypes[1].setupCallback
+	ENCHANTING.dataTypes[1].setupCallback = function(rowControl, slot)
+			hookedFunctions(rowControl, slot)
+			CreateMarkerControl(rowControl)
+		end
+
+	--research list hook
+	local hookedFunctions = LIST_DIALOG.dataTypes[1].setupCallback
+	LIST_DIALOG.dataTypes[1].setupCallback = function(rowControl, slot)
+			hookedFunctions(rowControl, slot)
+			CreateMarkerControl(rowControl)
+
+			local data = rowControl.dataEntry.data
+			local isSoulGem = false
+			local bagId, slotIndex = GetInfoFromRowControl(rowControl)
+			local signedItemInstanceId = SignItemId(GetItemInstanceId(bagId, slotIndex))
+
+			if(data and GetSoulGemItemInfo(data.bag, data.index) > 0) then
+				isSoulGem = true
+			end
+			if(ISSettings:IsFilterOn() and ISSettings:IsFilterResearch() and not isSoulGem and markedItems[signedItemInstanceId]) then
+				rowControl:SetMouseEnabled(false)
+				rowControl:GetNamedChild("Name"):SetColor(.75, 0, 0, 1)
+			else
+				rowControl:SetMouseEnabled(true)
+			end
+		end
+
+	--ZO_ScrollList_RefreshVisible(BACKPACK)
+	--ZO_ScrollList_RefreshVisible(BANK)
+	--ZO_ScrollList_RefreshVisible(GUILD_BANK)
+	RefreshAll()
+	ItemSaver_ToggleFilters(ISSettings:IsFilterOn())
+
+	ZO_CreateStringId("SI_BINDING_NAME_ITEM_SAVER_TOGGLE", "Toggle Item Saved")
+
+	SLASH_COMMANDS["/itemsaver"] = function(arg)
+			if(arg == "filters") then
+				ISSettings:ToggleFilter()
+				ItemSaver_ToggleFilters(ISSettings:IsFilterOn())
+			end
+		end
+end
+
+EVENT_MANAGER:RegisterForEvent("ItemSaverLoaded", EVENT_ADD_ON_LOADED, ItemSaver_Loaded)
---
+++ ItemSaver_Strings.lua
@@ -0,0 +1,101 @@
+ItemSaver_Strings = {
+	["de"] = {
+		--Added headlines for the settings in LAM 2.0
+		HEAD_GENERAL = "Generelle Optionen",
+		HEAD_FILTER = "Filter Optionen",
+ 
+        ICON_LABEL = "Symbol",
+        ICON_TOOLTIP = "Symbol der markierten Items",
+ 
+        TEXTURE_COLOR_LABEL = "Symbol Farbe",
+        TEXTURE_COLOR_TOOLTIP = "Farbe für das Symbol der markierten Items",
+ 
+        FILTERS_TOGGLE_LABEL = "Filter aktivieren",
+        FILTERS_TOGGLE_TOOLTIP = "Die Filter aktivieren/deaktivieren (\"/itemsaver filters\" Chat Befehl)",
+ 
+        FILTERS_SHOP_LABEL = "Shop filtern",
+        FILTERS_SHOP_TOOLTIP = "Sollen markierte Items von der Verkaufsliste entfernt werden?",
+ 
+        FILTERS_DECONSCRUCTION_LABEL = "Zerlegen filtern",
+        FILTERS_DECONSCRUCTION_TOOLTIP = "Sollen markierte Items von der Zerlegen-/Dekonstruktionsliste entfernt werden?",
+ 
+        FILTERS_RESEARCH_LABEL = "Analyse filtern",
+        FILTERS_RESEARCH_TOOLTIP = "Sollen markierte Items von der Analyseliste entfernt werden?",
+    },
+	["en"] = {
+		ICON_LABEL = "Icon Texture",
+		ICON_TOOLTIP = "Texture to use for your ItemSaver",
+
+		TEXTURE_COLOR_LABEL = "Texture Color",
+		TEXTURE_COLOR_TOOLTIP = "Color to use for ItemSaver's texture",
+
+		FILTERS_TOGGLE_LABEL = "Use Filters?",
+		FILTERS_TOGGLE_TOOLTIP = "Toggle the filters on and off (\"/itemsaver filters\" slash command)",
+
+		FILTERS_SHOP_LABEL = "Filter Shop?",
+		FILTERS_SHOP_TOOLTIP = "Should saved items be removed from the sell list?",
+
+		FILTERS_DECONSCRUCTION_LABEL = "Filter Deconstruction?",
+		FILTERS_DECONSCRUCTION_TOOLTIP = "Should saved items be removed from the deconstruction list?",
+
+		FILTERS_RESEARCH_LABEL = "Filter Research?",
+		FILTERS_RESEARCH_TOOLTIP = "Should saved items be removed from the research menu?",
+	},
+	["es"] = {
+		ICON_LABEL = "Icono",
+		ICON_TOOLTIP = "Icono para tu ItemSaver",
+
+		TEXTURE_COLOR_LABEL = "Color",
+		TEXTURE_COLOR_TOOLTIP = "Color que tendr195\161 tu icono ItemSaver",
+
+		FILTERS_TOGGLE_LABEL = "Aplicar filtros",
+		FILTERS_TOGGLE_TOOLTIP = "Activa/desactiva los filtros (\"/itemsaver filters\" slash command)",
+
+		FILTERS_SHOP_LABEL = "Filtrar venta",
+		FILTERS_SHOP_TOOLTIP = "Los objetos marcados no estar195\161n visibles en la ventana de venta",
+
+		FILTERS_DECONSCRUCTION_LABEL = "Filtrar desconstrucci195\179n",
+		FILTERS_DECONSCRUCTION_TOOLTIP = "Los objetos marcados no estar195\161n visibles en la ventana de desconstrucci195\179n",
+
+		FILTERS_RESEARCH_LABEL = "Filtrar investigaci195\161n",
+		FILTERS_RESEARCH_TOOLTIP = "Los objetos marcados no estar195\161n visibles en la ventana de investigaci195\161n",
+	},
+	["fr"] = {
+		ICON_LABEL = "Ic\195\180ne",
+		ICON_TOOLTIP = "Texture \195\160 utiliser avec ItemSaver",
+
+		TEXTURE_COLOR_LABEL = "Couleur",
+		TEXTURE_COLOR_TOOLTIP = "Couleur \195\160 appliquer \195\160 la texture d'ItemSaver",
+
+		FILTERS_TOGGLE_LABEL = "Utiliser les filtres?",
+		FILTERS_TOGGLE_TOOLTIP = "Active ou d\195\169sactive les filtres (commande \"/itemsaver filters\")",
+
+		FILTERS_SHOP_LABEL = "Filtrage au niveau des magasins?",
+		FILTERS_SHOP_TOOLTIP = "Si les articles sauvegard\195\169s doivent \195\170tre retir\195\169s de la liste des ventes?",
+
+		FILTERS_DECONSCRUCTION_LABEL = "Filtrage au niveau de la d\195\169construction?",
+		FILTERS_DECONSCRUCTION_TOOLTIP = "Si les articles sauvegard\195\169s doivent \195\170tre retir\195\169s de la liste de d\195\169construction?",
+
+		FILTERS_RESEARCH_LABEL = "Filtrage au niveau de la recherche?",
+		FILTERS_RESEARCH_TOOLTIP = "Si les articles sauvegard\195\169s doivent \195\170tre retir\195\169s du menu de recherche?",
+	},
+	["ru"] = {
+		ICON_LABEL = "Icon Texture",
+		ICON_TOOLTIP = "Texture to use for your ItemSaver",
+
+		TEXTURE_COLOR_LABEL = "Texture Color",
+		TEXTURE_COLOR_TOOLTIP = "Color to use for ItemSaver's texture",
+
+		FILTERS_TOGGLE_LABEL = "Use Filters?",
+		FILTERS_TOGGLE_TOOLTIP = "Toggle the filters on and off (\"/itemsaver filters\" slash command)",
+
+		FILTERS_SHOP_LABEL = "Filter Shop?",
+		FILTERS_SHOP_TOOLTIP = "Should saved items be removed from the sell list?",
+
+		FILTERS_DECONSCRUCTION_LABEL = "Filter Deconstruction?",
+		FILTERS_DECONSCRUCTION_TOOLTIP = "Should saved items be removed from the deconstruction list?",
+
+		FILTERS_RESEARCH_LABEL = "Filter Research?",
+		FILTERS_RESEARCH_TOOLTIP = "Should saved items be removed from the research menu?",
+	},
+}
