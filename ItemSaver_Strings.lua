c686b291bab03113fe15cb729b376eb5ff900626
Randall Fowler <rafowler@randactyl.com>
2015-03-07 13:29:56 -0800

    remove duplicate strings, set metatable fallbacks

--- ItemSaver_Strings.lua
+++ ItemSaver_Strings.lua
@@ -80,22 +80,10 @@ ItemSaver_Strings = {
 		FILTERS_RESEARCH_TOOLTIP = "Si les articles sauvegard\195\169s doivent \195\170tre retir\195\169s du menu de recherche?",
 	},
 	["ru"] = {
-		ICON_LABEL = "Icon Texture",
-		ICON_TOOLTIP = "Texture to use for your ItemSaver",
-
-		TEXTURE_COLOR_LABEL = "Texture Color",
-		TEXTURE_COLOR_TOOLTIP = "Color to use for ItemSaver's texture",
-
-		FILTERS_TOGGLE_LABEL = "Use Filters?",
-		FILTERS_TOGGLE_TOOLTIP = "Toggle the filters on and off (\"/itemsaver filters\" slash command)",
-
-		FILTERS_SHOP_LABEL = "Filter Shop?",
-		FILTERS_SHOP_TOOLTIP = "Should saved items be removed from the sell list?",
-
-		FILTERS_DECONSCRUCTION_LABEL = "Filter Deconstruction?",
-		FILTERS_DECONSCRUCTION_TOOLTIP = "Should saved items be removed from the deconstruction list?",
-
-		FILTERS_RESEARCH_LABEL = "Filter Research?",
-		FILTERS_RESEARCH_TOOLTIP = "Should saved items be removed from the research menu?",
 	},
 }
+
+setmetatable(ItemSaver_Strings["de"], {__index = ItemSaver_Strings["en"]})
+setmetatable(ItemSaver_Strings["fr"], {__index = ItemSaver_Strings["en"]})
+setmetatable(ItemSaver_Strings["ru"], {__index = ItemSaver_Strings["en"]})
+setmetatable(ItemSaver_Strings["es"], {__index = ItemSaver_Strings["en"]})
