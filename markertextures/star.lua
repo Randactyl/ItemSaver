3df6d8838e16a95f572a75919d3bc76236f0e985
Randactyl <rafowler@randactyl.com>
2015-07-31 12:05:24 -0700

    Add some base markers

---
+++ markertextures/star.lua
@@ -0,0 +1,6 @@
+local markerInformation = {
+    markerName = "Star",
+    texturePath = [[/esoui/art/campaign/overview_indexicon_bonus_disabled.dds]],
+}
+
+ItemSaver_RegisterMarker(markerInformation)
---
+++ markertextures/timer.lua
@@ -0,0 +1,6 @@
+local markerInformation = {
+    markerName = "Timer",
+    texturePath = [[/esoui/art/mounts/timer_icon.dds]],
+}
+
+ItemSaver_RegisterMarker(markerInformation)
