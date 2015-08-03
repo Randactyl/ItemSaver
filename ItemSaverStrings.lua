95bd3629640d2adee0fa659ef834656b726c75f4
Randactyl <rafowler@randactyl.com>
2015-08-03 15:12:25 -0700

    update strings

--- ItemSaverStrings.lua
+++ ItemSaverStrings.lua
@@ -2,26 +2,26 @@ local strings = {
 	["de"] = {
     },
 	["en"] = {
+		["SI_ITEMSAVER_ADDON_NAME"] = "Item Saver",
+
 		["SI_ITEMSAVER_GENERAL_OPTIONS_HEADER"] = "General Options",
 
 		["SI_ITEMSAVER_RELOAD_UI_WARNING"] = "Will reload UI",
-
 		["SI_ITEMSAVER_APPLY_CHANGES_BUTTON"] = "Apply Changes",
 		["SI_ITEMSAVER_APPLY_CHANGES_TOOLTIP"] = "Click this whenever you make a change below",
-
-		["SI_ITEMSAVER_ICON_ANCHOR_LABEL"] = "Icon Position",
-		["SI_ITEMSAVER_ICON_ANCHOR_TOOLTIP"] = "Position of the saved item marker",
+		["SI_ITEMSAVER_MARKER_ANCHOR_LABEL"] = "Marker Position",
+		["SI_ITEMSAVER_MARKER_ANCHOR_TOOLTIP"] = "Position of the saved item marker",
 
 		["SI_ITEMSAVER_SET_DATA_HEADER"] = "Set Data",
 
-		["SI_ITEMSAVER_ICON_LABEL"] = "Icon Texture",
-		["SI_ITEMSAVER_ICON_TOOLTIP"] = "Texture to use for your Item Saver",
+		["SI_ITEMSAVER_MARKER_LABEL"] = "Marker Texture",
+		["SI_ITEMSAVER_MARKER_TOOLTIP"] = "Texture to use for your Item Saver",
 
 		["SI_ITEMSAVER_TEXTURE_COLOR_LABEL"] = "Texture Color",
 		["SI_ITEMSAVER_TEXTURE_COLOR_TOOLTIP"] = "Color to use for Item Saver's texture",
 
-		["SI_ITEMSAVER_FILTERS_SHOP_LABEL"] = "Filter Shop?",
-		["SI_ITEMSAVER_FILTERS_SHOP_TOOLTIP"] = "Should saved items be removed from the vendor sell list?",
+		["SI_ITEMSAVER_FILTERS_STORE_LABEL"] = "Filter Store?",
+		["SI_ITEMSAVER_FILTERS_STORE_TOOLTIP"] = "Should saved items be removed from the vendor sell list?",
 
 		["SI_ITEMSAVER_FILTERS_DECONSTRUCTION_LABEL"] = "Filter Deconstruction?",
 		["SI_ITEMSAVER_FILTERS_DECONSTRUCTION_TOOLTIP"] = "Should saved items be removed from the deconstruction list?",
@@ -37,6 +37,14 @@ local strings = {
 
 		["SI_ITEMSAVER_FILTERS_TRADE_LABEL"] = "Filter Trade?",
 		["SI_ITEMSAVER_FILTERS_TRADE_TOOLTIP"] = "Should saved items be removed from the trade list?",
+
+		["SI_ITEMSAVER_DELETE_SET_BUTTON"] = "Delete Set",
+		["SI_ITEMSAVER_DELETE_SET_TOOLTIP"] = "Removes the set and changes any items in the set to the default set",
+
+		["SI_ITEMSAVER_CREATE_SAVE_SET"] = "+ Create Set",
+		["SI_ITEMSAVER_UNSAVE_ITEM"] = "Unsave item",
+
+		["SI_BINDING_NAME_ITEM_SAVER_TOGGLE"] = "Toggle Item Saved",
 	},
 	["es"] = {
 	},
