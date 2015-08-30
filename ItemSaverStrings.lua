9374077519ff15c939a20f3781f08ce596d18f70
Randactyl <rafowler@randactyl.com>
2015-08-29 18:26:51 -0700

    remove string metatables
    because they did not work how I thought they did and will not work in
    this way

--- ItemSaverStrings.lua
+++ ItemSaverStrings.lua
@@ -54,12 +54,6 @@ local strings = {
 	},
 }
 
---use metatables to set english as default language
-setmetatable(strings["de"], {__index = strings["en"]})
-setmetatable(strings["es"], {__index = strings["en"]})
-setmetatable(strings["fr"], {__index = strings["en"]})
-setmetatable(strings["ru"], {__index = strings["en"]})
-
 local function AddStringsToGame()
 	local lang = GetCVar("language.2")
 	if strings[lang] == nil then lang = "en" end
