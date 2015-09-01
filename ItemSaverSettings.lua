4cae24acaf1db13ac6f4dc881313c39c9ef3608d
Randactyl <rafowler@randactyl.com>
2015-09-01 13:09:23 -0700

    update addon version

--- ItemSaverSettings.lua
+++ ItemSaverSettings.lua
@@ -267,7 +267,7 @@ function ItemSaverSettings:CreateOptionsMenu()
 		type = "panel",
 		name = "Item Saver",
 		author = "Randactyl, ingeniousclown",
-		version = "2.0.0.0",
+		version = "2.0.1.0",
 		slashCommand = "/itemsaver",
 		registerForRefresh = true,
 	}
