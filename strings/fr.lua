262feb35d04e1e81f32c140ebe8dc8d81aa0fb0f
Randactyl <rafowler@randactyl.com>
2017-08-14 12:34:04 -0700

    added French translation from lexo1000

---
+++ strings/fr.lua
@@ -0,0 +1,95 @@
+SafeAddString(SI_ITEMSAVER_RELOAD_UI_WARNING, "Va entrainer un rechargement de l'interface", 1)
+
+SafeAddString(SI_ITEMSAVER_GENERAL_OPTIONS_HEADER, "Options générales", 1)
+
+SafeAddString(SI_ITEMSAVER_DEFAULT_SET_DROPDOWN_LABEL, "Set par défaut", 1)
+SafeAddString(SI_ITEMSAVER_DEFAULT_SET_DROPDOWN_TOOLTIP, "Détermine le set par défaut utilisé par la touche de raccourci.", 1)
+
+SafeAddString(SI_ITEMSAVER_MARKER_ANCHOR_LABEL, "Position du marqueur", 1)
+SafeAddString(SI_ITEMSAVER_MARKER_ANCHOR_TOOLTIP, "Détermine la position du marqueur sur l'icône d'objet.", 1)
+SafeAddString(SI_ITEMSAVER_ANCHOR_LABEL_TOPLEFT, "En haut à gauche", 1)
+SafeAddString(SI_ITEMSAVER_ANCHOR_LABEL_TOP, "En haut", 1)
+SafeAddString(SI_ITEMSAVER_ANCHOR_LABEL_TOPRIGHT, "En haut à droite", 1)
+SafeAddString(SI_ITEMSAVER_ANCHOR_LABEL_RIGHT, "À droite", 1)
+SafeAddString(SI_ITEMSAVER_ANCHOR_LABEL_BOTTOMRIGHT, "En bas à droite", 1)
+SafeAddString(SI_ITEMSAVER_ANCHOR_LABEL_BOTTOM, "En bas", 1)
+SafeAddString(SI_ITEMSAVER_ANCHOR_LABEL_BOTTOMLEFT, "En bas à gauche", 1)
+SafeAddString(SI_ITEMSAVER_ANCHOR_LABEL_LEFT, "À gauche", 1)
+SafeAddString(SI_ITEMSAVER_ANCHOR_LABEL_CENTER, "Au centre", 1)
+
+SafeAddString(SI_ITEMSAVER_OFFSET_X_SLIDER, "Décalage horizontal", 1)
+SafeAddString(SI_ITEMSAVER_OFFSET_X_TOOLTIP, "Détermine le décalage horizontal appliqué au marqueur.", 1)
+SafeAddString(SI_ITEMSAVER_OFFSET_Y_SLIDER, "Décalage vertical", 1)
+SafeAddString(SI_ITEMSAVER_OFFSET_Y_TOOLTIP, "Détermine le décalage vertical appliqué au marqueur.", 1)
+
+SafeAddString(SI_ITEMSAVER_DEFER_SUBMENU_CHECKBOX_LABEL, "Supprimer l'affichage du sous-menu", 1)
+SafeAddString(SI_ITEMSAVER_DEFER_SUBMENU_CHECKBOX_TOOLTIP, "Affiche un sous-menu uniquement si le nombre de set est supérieur au nombre indiqué.", 1)
+
+SafeAddString(SI_ITEMSAVER_DEFER_SUBMENU_DROPDOWN_LABEL, "Nombre de marqueurs", 1)
+SafeAddString(SI_ITEMSAVER_DEFER_SUBMENU_DROPDOWN_TOOLTIP, "Détermine le nombre de sets minimum pour que le sous-menu s'affiche.", 1)
+
+SafeAddString(SI_ITEMSAVER_KEYBIND_SUBMENU_NAME, "Keybind Options", 1)
+SafeAddString(SI_ITEMSAVER_KEYBIND_SET_1_NAME, "Set 1", 1)
+SafeAddString(SI_ITEMSAVER_KEYBIND_SET_2_NAME, "Set 2", 1)
+SafeAddString(SI_ITEMSAVER_KEYBIND_SET_3_NAME, "Set 3", 1)
+SafeAddString(SI_ITEMSAVER_KEYBIND_SET_4_NAME, "Set 4", 1)
+SafeAddString(SI_ITEMSAVER_KEYBIND_SET_5_NAME, "Set 5", 1)
+
+SafeAddString(SI_ITEMSAVER_SET_DATA_HEADER, "Paramètres du set", 1)
+
+SafeAddString(SI_ITEMSAVER_EDIT_SET_DROPDOWN_LABEL, "Paramètres du set", 1)
+SafeAddString(SI_ITEMSAVER_EDIT_SET_DROPDOWN_TOOLTIP, "Le set sélectionné est configurable via les options ci-dessous.", 1)
+
+SafeAddString(SI_ITEMSAVER_SAVE_TYPE_DROPDOWN_LABEL, "Type de set", 1)
+SafeAddString(SI_ITEMSAVER_SAVE_TYPE_DROPDOWN_TOOLTIP, "Modifie la façon dont les objets liés à ce set réagissent. Le réglage 'Général' marque toutes les piles de l'objet sélectionné. 'Unique' marque seulement l'objet sélectionné et persistera si cet objet subit un changement tel qu'un enchantement ou une amélioration.", 1)
+SafeAddString(SI_ITEMSAVER_SAVE_TYPE_DROPDOWN_WARNING, "Modifier cette option retirera tous les objets liés à ce set.", 1)
+
+SafeAddString(SI_ITEMSAVER_SAVE_TYPE_DROPDOWN_GENERAL, "Général", 1)
+SafeAddString(SI_ITEMSAVER_SAVE_TYPE_DROPDOWN_UNIQUE, "Unique", 1)
+
+SafeAddString(SI_ITEMSAVER_MARKER_LABEL, "Apparence du marqueur", 1)
+SafeAddString(SI_ITEMSAVER_MARKER_TOOLTIP, "Détermine l'apparence du marqueur utilisé par ce set.", 1)
+
+SafeAddString(SI_ITEMSAVER_TEXTURE_COLOR_LABEL, "Couleur du marqueur", 1)
+SafeAddString(SI_ITEMSAVER_TEXTURE_COLOR_TOOLTIP, "Détermine la couleur du marqueur utilisé par ce set.", 1)
+
+SafeAddString(SI_ITEMSAVER_FILTERS_VENDORSELL_LABEL, "Retirer du magasin", 1)
+SafeAddString(SI_ITEMSAVER_FILTERS_VENDORSELL_TOOLTIP, "Les objets liés à ce set n'apparaîtront pas dans la liste des objets à vendre.", 1)
+
+SafeAddString(SI_ITEMSAVER_FILTERS_SMITHINGDECONSTRUCT_LABEL, "Retirer de la déconstruction", 1)
+SafeAddString(SI_ITEMSAVER_FILTERS_SMITHINGDECONSTRUCT_TOOLTIP, "Les objets liés à ce set n'apparaîtront pas dans la liste de déconstruction.", 1)
+
+SafeAddString(SI_ITEMSAVER_FILTERS_SMITHINGRESEARCH_LABEL, "Retirer de la recherche", 1)
+SafeAddString(SI_ITEMSAVER_FILTERS_SMITHINGRESEARCH_TOOLTIP, "Les objets liés à ce set n'apparaîtront pas dans la liste de recherche.", 1)
+
+SafeAddString(SI_ITEMSAVER_FILTERS_GUILDSTORESELL_LABEL, "Retirer du magasin de guilde", 1)
+SafeAddString(SI_ITEMSAVER_FILTERS_GUILDSTORESELL_TOOLTIP, "Les objets liés à ce set n'apparaîtront pas dans le magasin de guilde", 1)
+
+SafeAddString(SI_ITEMSAVER_FILTERS_MAILSEND_LABEL, "Retirer du courrier", 1)
+SafeAddString(SI_ITEMSAVER_FILTERS_MAILSEND_TOOLTIP, "Les objets liés à ce set n'apparaîtront pas dans les pièces jointes du courrier.", 1)
+
+SafeAddString(SI_ITEMSAVER_FILTERS_TRADE_LABEL, "Retirer des échanges", 1)
+SafeAddString(SI_ITEMSAVER_FILTERS_TRADE_TOOLTIP, "Les objets liés à ce set n'apparaîtront pas dans la liste d'échange.", 1)
+
+SafeAddString(SI_ITEMSAVER_CLEAR_SET_BUTTON, "Réinitialiser le set", 1)
+SafeAddString(SI_ITEMSAVER_CLEAR_SET_TOOLTIP, "Retire de ce set tous les objets qui lui sont lié.", 1)
+SafeAddString(SI_ITEMSAVER_CLEAR_SET_CONFIRMATION, "Retirer tous les objets du marquage", 1)
+
+SafeAddString(SI_ITEMSAVER_DELETE_SET_BUTTON, "Supprimer le set", 1)
+SafeAddString(SI_ITEMSAVER_DELETE_SET_TOOLTIP, "Retire de ce set tous les objets qui lui sont liés et supprime le set.", 1)
+
+SafeAddString(SI_ITEMSAVER_CREATE_SAVE_SET, "+ 'Créer un set'", 1)
+SafeAddString(SI_ITEMSAVER_UNSAVE_ITEM, "Retirer le marqueur", 1)
+SafeAddString(SI_ITEMSAVER_SAVE_TO, ">", 1)
+
+SafeAddString(SI_ITEMSAVER_SET_NAME_LABEL, "Nom", 1)
+SafeAddString(SI_ITEMSAVER_SET_NAME_TOOLTIP, "Détermine le nom du nouveau set.", 1)
+SafeAddString(SI_ITEMSAVER_MISSING_NAME_WARNING, "Set non sauvegardé - aucun nom n'est attribué au set.", 1)
+SafeAddString(SI_ITEMSAVER_USED_NAME_WARNING, "Set non sauvegardé - le nom est déjà attribué.", 1)
+SafeAddString(SI_ITEMSAVER_SELECT_FILTERS_LABEL, "Filtres", 1)
+
+SafeAddString(SI_BINDING_NAME_ITEM_SAVER_TOGGLE_1, "Enregistrer dans le set 1", 1)
+SafeAddString(SI_BINDING_NAME_ITEM_SAVER_TOGGLE_2, "Enregistrer dans le set 2", 1)
+SafeAddString(SI_BINDING_NAME_ITEM_SAVER_TOGGLE_3, "Enregistrer dans le set 3", 1)
+SafeAddString(SI_BINDING_NAME_ITEM_SAVER_TOGGLE_4, "Enregistrer dans le set 4", 1)
+SafeAddString(SI_BINDING_NAME_ITEM_SAVER_TOGGLE_5, "Enregistrer dans le set 5", 1)
