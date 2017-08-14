e85d664b3143af3deb0c1df27c96eb6171c2652c
Randactyl <rafowler@randactyl.com>
2017-08-14 12:13:34 -0700

    update LibFilters to version 2.0r3.1

--- lib/LibFilters-2.0/helper.lua
+++ lib/LibFilters-2.0/helper.lua
@@ -135,14 +135,12 @@ helpers["REPAIR_WINDOW"] = {
 }
 
 --enable LF_ALCHEMY_CREATION, LF_ENCHANTING_CREATION, LF_ENCHANTING_EXTRACTION,
---  LF_SMITHING_REFINE, LF_SMITHING_DECONSTRUCT, LF_SMITHING_IMPROVEMENT
+--  LF_SMITHING_REFINE
 helpers["enumerate"] = {
-    version = 1,
+    version = 2,
     locations = {
         [1] = ZO_AlchemyInventory,
         [2] = ZO_EnchantingInventory,
-        [3] = ZO_SmithingExtractionInventory,
-        [4] = ZO_SmithingImprovementInventory,
     },
     helper = {
         funcName = "EnumerateInventorySlotsAndAddToScrollData",
@@ -158,6 +156,8 @@ helpers["enumerate"] = {
                 return oldPredicate(bagId, slotIndex) and result
             end
 
+            -- Begin original function
+
             local list = PLAYER_INVENTORY:GenerateListOfVirtualStackedItems(INVENTORY_BACKPACK, predicate)
             PLAYER_INVENTORY:GenerateListOfVirtualStackedItems(INVENTORY_BANK, predicate, list)
             PLAYER_INVENTORY:GenerateListOfVirtualStackedItems(INVENTORY_CRAFT_BAG, predicate, list)
@@ -166,12 +166,52 @@ helpers["enumerate"] = {
 
             for itemId, itemInfo in pairs(list) do
                 if not filterFunction or filterFunction(itemInfo.bag, itemInfo.index, filterType) then
-                    self:AddItemData(itemInfo.bag, itemInfo.index, itemInfo.stack, self:GetScrollDataType(itemInfo.bag, itemInfo.index), data, self.customDataGetFunction, validItemIds)
+                    self:AddItemData(itemInfo.bag, itemInfo.index, itemInfo.stack, self:GetScrollDataType(itemInfo.bag, itemInfo.index), data, self.customDataGetFunction)
                 end
                 self.itemCounts[itemId] = itemInfo.stack
             end
 
             return list
+        end,
+    },
+}
+
+--enable LF_SMITHING_DECONSTRUCT, LF_SMITHING_IMPROVEMENT
+helpers["GetIndividualInventorySlotsAndAddToScrollData"] = {
+    version = 1,
+    locations = {
+        [1] = ZO_SmithingExtractionInventory,
+        [2] = ZO_SmithingImprovementInventory,
+    },
+    helper = {
+        funcName = "GetIndividualInventorySlotsAndAddToScrollData",
+        func = function(self, predicate, filterFunction, filterType, data, useWornBag)
+            local oldPredicate = predicate
+            predicate = function(bagId, slotIndex)
+                local result = true
+
+                if type(self.additionalFilter) == "function" then
+                    result = self.additionalFilter(bagId, slotIndex)
+                end
+
+                return oldPredicate(bagId, slotIndex) and result
+            end
+
+            -- Begin original function
+
+            local bagsToUse = useWornBag and ZO_ALL_CRAFTING_INVENTORY_BAGS_AND_WORN or ZO_ALL_CRAFTING_INVENTORY_BAGS_WITHOUT_WORN
+            local filteredDataTable = SHARED_INVENTORY:GenerateFullSlotData(predicate, unpack(bagsToUse))
+
+            ZO_ClearTable(self.itemCounts)
+
+            for i, slotData in pairs(filteredDataTable) do
+                if not filterFunction or filterFunction(slotData.bagId, slotData.slotIndex, filterType) then
+                    self:AddItemData(slotData.bagId, slotData.slotIndex, slotData.stackCount, self:GetScrollDataType(slotData.bagId, slotData.slotIndex), data, self.customDataGetFunction, slotData)
+                end
+                self.itemCounts[i] = slotData.stackCount
+            end
+
+            return filteredDataTable
         end,
     },
 }
